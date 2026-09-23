2ad6a0a7242ebcd9f84cca40b5652be94ca78921
lgarreffa258-code <lgarreffa258@gmail.com>
2026-09-22 22:07:07 -0400

    made space ship
    made trauma mobile

--- topics/prototypes/instructions-prototype-2/template-p5-project/js/script.js
+++ topics/prototypes/instructions-prototype-2/template-p5-project/js/script.js
@@ -13,7 +13,11 @@
 function setup() {
     createCanvas(1000, 1000);
 }
-
+let sunX = {
+    r: 255,
+    g: 125,
+    b: 37
+};
 
 /**
  * drawing the scene
@@ -21,12 +25,16 @@ function setup() {
 function draw() {
     background("#000000");
 
+
     //sun
     push();
-    fill("#FF7D25");
-    stroke("#bb740a");
+    fill(sunX.r, sunX.g, sunX.b);
     circle(500, 300, 500);
     pop()
+    sunX.g = sunX.g - 1;
+    sunX.b = sunX.b - 3;
+
+
     //giants deep basically water planet
 
     push();
@@ -34,6 +42,97 @@ function draw() {
     stroke("#0A2B3D");
     circle(250, 750, 200);
     pop()
+
+    push();
+    fill("#a75231");
+    circle(900, 550, 100);
+    pop()
+
+    push();
+    fill("#a75231");
+    circle(800, 650, 150);
+    pop()
+
+    push();
+    fill("#020100");
+    circle(550, 150, 100);
+    pop()
+
+    // the ship from outer wilds, aka the trauma mobile
+
+    //awesome landing gear
+    push();
+    stroke("#2d3436");
+    strokeWeight(5);
+    line(460, 530, 430, 590);
+    line(415, 590, 445, 590);
+    line(540, 530, 570, 590);
+    line(555, 590, 585, 590);
+    pop();
+
+    //thruster flames
+    push();
+    noStroke();
+    fill("#ff7675");
+    triangle(442, 570, 463, 570, 452, 600);
+    triangle(537, 570, 558, 570, 547, 600);
+    pop();
+
+    //side fuel tanks
+    push();
+    stroke("#2d3436");
+    strokeWeight(3);
+    fill("#b2bec3");
+    rect(440, 510, 25, 60, 8);
+    rect(535, 510, 25, 60, 8);
+    pop();
+
+    //fuel tank caps
+    push();
+    fill("#7f8c8d");
+    rect(440, 510, 25, 12, 8, 8, 0, 0);
+    rect(535, 510, 25, 12, 8, 8, 0, 0);
+    pop();
+
+    //makeshift duct tape strapping the tanks
+    push();
+    noStroke();
+    fill("#95a5a6");
+    rect(455, 505, 20, 8);
+    rect(525, 505, 20, 8);
+    pop();
+
+    // main cockpit capsule weird woden ball
+    push();
+    stroke("#3d1f10");
+    strokeWeight(4);
+    fill("#8b4513");
+    circle(500, 500, 90);
+    pop();
+
+    // 7. horizontal metal hull strap
+    push();
+    fill("#636e72");
+    noStroke();
+    rect(457, 496, 86, 8);
+    pop();
+
+    // 8. cyan visor window
+    push();
+    stroke("#2d3436");
+    strokeWeight(3);
+    fill("#00cec9");
+    rect(480, 470, 40, 26, 12, 12, 6, 6);
+    pop();
+
+
+    time.sleep(1);
+    sunX.r = sunX.r - 5;
+    sunX.b = sunX.b + 5;
+
+
+
+
 
 
 }

ff2edbb34ee9d66b6b75f0e80bae4dec90651ff3
lgarreffa258-code <lgarreffa258@gmail.com>
2026-09-22 20:37:58 -0400

    planets
    added big water and hot fire

--- topics/prototypes/instructions-prototype-2/template-p5-project/js/script.js
+++ topics/prototypes/instructions-prototype-2/template-p5-project/js/script.js
@@ -1,24 +1,39 @@
 /**
- * Title of Project
- * Author Name
+ * outer wilds
+ * Luca Garreffa
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * a tribute to the game outer wilds, this is a simple scene of a planet with a sun in the background.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * making the canvas
 */
 function setup() {
-
+    createCanvas(1000, 1000);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * drawing the scene
+ */
 function draw() {
+    background("#000000");
+
+    //sun
+    push();
+    fill("#FF7D25");
+    stroke("#bb740a");
+    circle(500, 300, 500);
+    pop()
+    //giants deep basically water planet
+
+    push();
+    fill("#58615a");
+    stroke("#0A2B3D");
+    circle(250, 750, 200);
+    pop()
+
 
 }

4e50e815c103434c52b34f0aa861e80bf6ef196e
lgarreffa258-code <lgarreffa258@gmail.com>
2026-09-22 23:04:08 -0400

    added prototypes
    added everything to my site

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 welcome to my CART253 course website. this website is to collect together and show off my prototyping work in CART253
 ## Prototypes
 
+[Outer Wilds](images/outer%20wilds.png)
+. This picture is a tribute to the game outer wilds. this tribute obviously does not do it justice but its an amazing game based on [space.](http://127.0.0.1:5500/topics/prototypes/instructions-prototype-2/template-p5-project/)
+
+[The last sunset](images/last%20sunset.png)
+. This one is meant to be kinda like when poeple talk about bleeding out in the snow watching the orange sun go down for the last time knowing that everything will be ok, they don't have to worry [anymore.]()
 ## Links
 [Youtube](https://www.youtube.com/@Lucagarreffazoom) 
 ## Journal
--- topics/prototypes/instructions-prototype-3/template-p5-project/js/script.js
+++ topics/prototypes/instructions-prototype-3/template-p5-project/js/script.js
@@ -1,24 +1,48 @@
 /**
- * Title of Project
- * Author Name
+ * smiley face
+ * Luca Garreffa
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *a big smiley face
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * making the canvas
 */
 function setup() {
-
+    createCanvas(1000, 1000);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * drawing the scene
 */
 function draw() {
+    background("#dd8a15");
 
+    push();
+    fill("#c1f700");
+    circle(500, 500, 500);
+    pop()
+
+    push();
+    fill("#0a0101");
+    ellipse(400, 400, 50, 100);
+    pop()
+
+    push();
+    fill("#0a0101");
+    ellipse(600, 400, 50, 100);
+    pop()
+
+    push();
+    fill("#020202");
+    arc(500, 600, 200, 100, 0, PI);
+    pop()
+
+    push();
+    fill("#020202");
+    arc(500, 600, 200, 100, 0, PI);
+    pop()
 }
